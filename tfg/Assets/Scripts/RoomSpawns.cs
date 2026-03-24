e87db4cd679c299e106e5143076dc34189a24421
saraidl14 <habladora016@gmail.com>
2026-03-24 11:07:02 +0100

    Creación Mazmorras
    Generación procedural de mazmorras

---
+++ tfg/Assets/Scripts/RoomSpawns.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawns : MonoBehaviour
+{
+    public int openSides;
+    // 0 = no open sides
+    // 1 = bottom
+    // 2 = top
+    // 3 = right
+    // 4 = left
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Spawn()
+    {
+        if (openSides == 1)
+        {
+        }
+        else if (openSides == 2)
+        {
+        }
+        else if (openSides == 3)
+        {
+        }
+        else if (openSides == 4)
+        {
+
+        }
+    }
+
+}
