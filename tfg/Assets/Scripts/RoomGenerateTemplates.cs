785ea459bf5b504c6956b045cca27ab1f4008a10
saraidl14 <habladora016@gmail.com>
2026-03-25 00:13:53 +0100

    Enemies creation
    Se han creado los primeros enemigos y una bola que da vida.

--- tfg/Assets/Scripts/RoomGenerateTemplates.cs
+++ tfg/Assets/Scripts/RoomGenerateTemplates.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class RoomGenerateTemplates : MonoBehaviour
 {
@@ -12,4 +13,30 @@ public class RoomGenerateTemplates : MonoBehaviour
     public GameObject closedRoom;
 
     public List<GameObject> rooms;
+
+    public GameObject boss;
+    public GameObject slime;
+    public GameObject skeleton;
+    public GameObject goblin;
+    public GameObject health;
+
+    private void Start()
+    {
+        Invoke("SpawnEnemies", 0.5f);
+        Invoke("SpawnHelps", 0.5f);
+    }
+
+    private void SpawnEnemies()
+    {
+        Instantiate(boss, rooms[rooms.Count - 1].transform.position, Quaternion.identity);
+
+        for(int i = 0; i < rooms.Count-1 ; i++)
+        {
+            Instantiate(slime, rooms[i].transform.position, Quaternion.identity);
+        }
+    }
+    private void SpawnHelps()
+    {
+
+    }
 }
