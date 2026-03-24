8a51aff2086ac214e59ec097fe4dc0128ec6a825
saraidl14 <habladora016@gmail.com>
2026-03-25 00:45:32 +0100

    Controlador enemigos

---
+++ tfg/Assets/Scripts/Enemies/BossController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : EnemyBase
+{
+    // Evento que se dispara cuando el boss muere (para la transicion de sala)
+    public static event System.Action OnBossDead;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        maxHealth = 300f;
+        damage = 25f;
+        moveSpeed = 3.5f;
+        attackRange = 2f;
+        attackCooldown = 2f;
+        currentHealth = maxHealth;
+    }
+
+    // Comportamiento: persigue y ataca. Preparado para BT/DDA mas adelante
+    protected override void UpdateBehavior()
+    {
+        if (PlayerInRange())
+        {
+            if (Time.time >= lastAttackTime + attackCooldown)
+                Attack();
+        }
+        else
+        {
+            MoveTowardsPlayer();
+        }
+    }
+
+    protected override void Attack()
+    {
+        lastAttackTime = Time.time;
+
+        PlayerHealth playerHealth = player.GetComponent<PlayerHealth>();
+        if (playerHealth != null)
+            playerHealth.TakeDamage(damage);
+    }
+
+    protected override void Die()
+    {
+        OnBossDead?.Invoke(); // Avisa al sistema de salas para hacer la transicion
+        base.Die();
+    }
+}
