87c9693bc508bbccbac12e9048fb6957f1fb3af3
saraidl14 <habladora016@gmail.com>
2026-03-24 23:50:27 +0100

    Corregido bug salas infinitas
    Se ha corregido de bug salas infinitas y ahora se generan proceduralmente hasta un numero finito de habitaciones segun las habitaciones dadas

--- tfg/Assets/Scripts/Destruction.cs
+++ tfg/Assets/Scripts/Destruction.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Destruction : MonoBehaviour
 {
-    void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
             Destroy(other.gameObject);
     }
